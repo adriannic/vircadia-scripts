301d0ab7b6a2325ed3cf10aab050e52d19d74c74
adriannic <52802806+adriannic@users.noreply.github.com>
2022-12-01 18:05:31 +0100

    Add files via upload

---
+++ seat.js
@@ -0,0 +1,81 @@
+(function() {
+	var seated = false;
+	var seater = Uuid.NULL;
+
+	function sit(entityID) {
+		Entities.editEntity(entityID, {
+			color: {
+				red: 0,
+				green: 255,
+				blue: 0
+			}
+		});
+		MyAvatar.beginSit(Entities.localToWorldPosition(Vec3.ZERO, entityID), Quat.cancelOutRoll(Quat.getFront(this.orientation)));
+		seated = true;
+		seater = MyAvatar.sessionUUID;
+	};
+
+	function unsit(entityID) {
+		Entities.editEntity(entityID, {
+			color: {
+				red: 255,
+				green: 0,
+				blue: 0
+			}
+		});
+		MyAvatar.endSit(Entities.localToWorldPosition(Vec3.ZERO, entityID), Quat.cancelOutRoll(Quat.getFront(this.orientation)));
+		seated = false;
+		seater = Uuid.NULL;
+	};
+
+	function trySit(entityID) {
+		var dist = Vec3.distance(Vec3.ZERO, Entities.worldToLocalPosition(Entities.localToWorldPosition(Vec3.ZERO, MyAvatar.sessionUUID), entityID));
+		var color = Entities.getEntityProperties(entityID).color;
+		print(color);
+		print(color.red);
+		print(color.green);
+		print(color.blue);
+		print();
+		if (Entities.getEntityProperties(entityID).color !== {red: 255, green: 0, blue: 0}) return;
+		if (dist > 25) return;
+		if (seated != MyAvatar.isSeated() || ((seated && MyAvatar.isSeated()) && seater !== MyAvatar.sessionUUID))
+			return;
+
+		if (seated) {
+			unsit(entityID);
+		} else {
+			sit(entityID);
+		}
+	}
+
+	this.preload = function(entityID) {
+		seated = false;
+		seater = 0;
+		Entities.editEntity(entityID, {
+			color: {
+				red: 255,
+				green: 0,
+				blue: 0
+			}
+		});
+	};
+
+	this.unload = function(entityID) {
+		if (seater === MyAvatar.sessionUUID) {
+			unsit(entityID);
+			seated = false;
+			seater = 0;
+		}
+		Entities.editEntity(entityID, {
+			color: {
+				red: 255,
+				green: 0,
+				blue: 0
+			}
+		});
+	};
+
+	this.mousePressOnEntity = function(entityID, mouseEvent) {
+		trySit(entityID);
+	};
+});
